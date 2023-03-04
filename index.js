c413fb4fafe45f781113707f319bf9f50c55273c
ouhab <mohamed-moh98@live.com>
2023-03-05 00:08:05 +0100

    Add chekout with stripe

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 
 app.listen(process.env.PORT || 5000, () =>{
