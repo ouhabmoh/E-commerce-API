30229824f919705d04cb6bb62e65128560bfea42
ouhab <mohamed-moh98@live.com>
2023-03-04 23:53:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL)
+        .then(() => console.log("db"))
+        .catch((e) => console.log(e));
+
+
+        app.get("/api/test", () => {
+            console.log("test is succ")
+        })
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, () =>{
+    console.log("backend is running");
+});
