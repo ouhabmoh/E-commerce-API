59a854a2d0904c9982a4ce0e9f60c13dc73326d3
ouhab <mohamed-moh98@live.com>
2023-03-05 00:06:57 +0100

    Add Order CRUD

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
-
+app.use("/api/orders", orderRoute);
 
 
 app.listen(process.env.PORT || 5000, () =>{
