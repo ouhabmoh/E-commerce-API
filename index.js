ffdaea7228746a8e61bb14e81bacc68ac3502d2a
ouhab <mohamed-moh98@live.com>
2023-03-04 23:57:32 +0100

    Add Prodcut CRUD

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 app.listen(process.env.PORT || 5000, () =>{
     console.log("backend is running");
